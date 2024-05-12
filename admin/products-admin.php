596e30abe98f75e2f5eb776ffbd35a1b359c7a84
truongnhat140104 <elvis140104@gmail.com>
2024-05-12 21:00:16 +0700

    sua

--- admin/products-admin.php
+++ admin/products-admin.php
@@ -4,79 +4,66 @@ global $conn;
 $username_admin = $_GET["admin"];
 ?>
 <span style="font-family: verdana, geneva, sans-serif;">
-  <!DOCTYPE html>
-  <html lang="en">
-
-  <head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
     <title>WEB ADMIN</title>
     <link rel="stylesheet" href="style/style_admin.css" />
     <link rel="stylesheet" href="style/style_product-admin.css">
-
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
   </head>
 
-  <body>
-
+<body>
     <div class="container">
-      <?php include 'navbar.php'; ?>
+        <?php include 'navbar.php'; ?>
         <?php
-        $sql = "SELECT sanpham.id_sp, sanpham.tensp, sanpham.image_sp, sanpham.gia, sanpham.motangan, danhmucsp.tendanhmuc FROM sanpham INNER JOIN danhmucsp on sanpham.id_danhmuc = danhmucsp.id_danhmuc";
+        $sql = "SELECT sanpham.id_sp, sanpham.tensp, sanpham.image_sp, sanpham.gia, sanpham.motangan, danhmucsp.tendanhmuc FROM sanpham INNER JOIN danhmucsp on sanpham.id_danhmuc = danhmucsp.id_danhmuc order by sanpham.id_sp";
         $result = $conn->query($sql);
         ?>
-      <!-- top banner -->
-      <div class="top-banner">
-        <p>Online Store</p>
-      </div>
+        <!-- top banner -->
+        <div class="top-banner">
+            <p>Online Store</p>
+        </div>
 
-      <section class="main">
+        <section class="main">
         <div class="main-top"></div>
         <div class="background-section">
-          <div class="main-body">
-            <h1 style="text-transform: uppercase;">chi tiết sản phẩm</h1>
-          </div>
-          <form action="" method="post">
-          <table>
-            <tr>
-              <th class="masanpham">Mã sản phẩm</th>
-              <th class="planloai">Phân loại</th>
-              <th class="anh">Ảnh</th>
-              <th class="name">Tên sản phẩm</th>
-              <th class="gia">Giá</th>
-              <th class="mota">Mô tả</th>
-              <th class="chucnang" colspan="2">Chức năng</th>
-            </tr>
-            <?php
-                while ($row = mysqli_fetch_array($result)) {
-            ?>
-            <tr>
-              <td class="masanpham"> <?php echo $row['id_sp'] ?> </td>
-              <td class="phanloai"><?php echo $row['tendanhmuc'] ?></td>
-              <td class="anh">
-                <img src="image/<?php echo $row['image_sp'] ?>" alt="" width="80px" height="80px">
-              </td>
-              <td class="name"><?php echo $row['tensp'] ?></td>
-              <td class="gia"><?php echo $row['gia'] ?>/cái</td>
-              <td class="mota"><?php echo $row['motangan'] ?></td>
-              <td class="chucnang">
-                <a href="suasanpham.php?admin=<?php echo $username_admin?>" style="color: blue;">Sửa</a>
-              </td>
-              <td class="xoa" onclick="buttonXoa()">Xóa</td>
-            </tr>
-            <?php } ?>
-            <script>
-              function buttonXoa() {
-                if (confirm("Bạn có muốn xóa sản phẩm không ?")) {
-                  alert("Đã xóa sản phẩm thành công!!!");
-                }
-              }
-            </script>
-          </table>
+            <div class="main-body">
+                <h1 style="text-transform: uppercase;">chi tiết sản phẩm</h1>
+            </div>
+            <form action="" method="post">
+                <table>
+                    <tr>
+                        <th class="masanpham">Mã sản phẩm</th>
+                        <th class="planloai">Phân loại</th>
+                        <th class="anh">Ảnh</th>
+                        <th class="name">Tên sản phẩm</th>
+                        <th class="gia">Giá</th>
+                        <th class="mota">Mô tả</th>
+                        <th class="chucnang" colspan="2">Chức năng</th>
+                    </tr>
+                    <?php
+                        while ($row = mysqli_fetch_array($result)) {
+                    ?>
+                    <tr>
+                        <td class="masanpham"> <?php echo $row['id_sp'] ?> </td>
+                        <td class="phanloai"><?php echo $row['tendanhmuc'] ?></td>
+                        <td class="anh">
+                            <img src="image/<?php echo $row['image_sp'] ?>" alt="image" width="80px" height="80px">
+                        </td>
+                        <td class="name" style="width: 15%"><?php echo $row['tensp']?></td>
+                        <td class="gia"><?php echo $row['gia'].'€' ?>/cái</td>
+                        <td class="mota"><?php echo $row['motangan'] ?></td>
+                        <td class="chucnang">
+                            <a href="suasanpham.php?admin=<?php echo $username_admin?>&id_sp=<?php echo $row['id_sp'] ?>" style="color: blue;">Sửa</a>
+                        </td>
+                        <td class="xoa">Xóa</td>
+                    </tr>
+                    <?php } ?>
+                </table>
             </form>
         </div>
-      </section>
+        </section>
     </div>
-  </body>
-
-  </html>
+</body>
+</html>
--- admin/suasanpham.php
+++ admin/suasanpham.php
@@ -7,107 +7,98 @@ if(isset($_POST["submit"])){
 }
 ?>
 <span style="font-family: verdana, geneva, sans-serif;">
-  <!DOCTYPE html>
-  <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-  <head>
+<head>
     <title>WEB ADMIN</title>
     <link rel="stylesheet" href="style/style_admin.css" />
     <link rel="stylesheet" href="style/style_edit-products.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
 
-  </head>
-
-  <body>
+<body>
 
     <div class="container">
-      <?php include 'navbar.php'; ?>
+    <?php include 'navbar.php'; ?>
 
 
-      <!-- top banner -->
-      <div class="top-banner">
+    <!-- top banner -->
+    <div class="top-banner">
         <p>online store</p>
-      </div>
+    </div>
 
-      <section class="main">
+    <section class="main">
         <div class="main-top"></div>
         <div class="background-section">
-          <div class="main-body">
-            <h1 style="text-transform: uppercase;">chỉnh sửa sản phẩm</h1>
-          </div>
-          <div class="main-body">
-            <img src="image/bed1.jpg" alt="add image">
-            <form action="" style="padding-bottom: 30px;">
-              <input type="file" name="uploadfile" id="img" style="display: none;">
-              <label for="img" class="img">
-                <span class="add-image">Sửa hình ảnh</span>
-              </label>
+            <div class="main-body">
+                <h1 style="text-transform: uppercase;">chỉnh sửa sản phẩm</h1>
+            </div>
+            <div class="main-body">
+                <img src="image/bed1.jpg" alt="add image">
+                <form action="" style="padding-bottom: 30px;">
+                    <input type="file" name="uploadfile" id="img" style="display: none;">
+                    <label for="img" class="img">
+                        <span class="add-image">Sửa hình ảnh</span>
+                    </label>
+                </form>
+            </div>
+            <form action="" method="">
+                <table>
+                    <tr>
+                        <th>Phân loại</th>
+                        <th>Tên sản phẩm</th>
+                        <th>Số lượng</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <select name="" id="" class='form-control'>
+                                <option value="">Giường</option>
+                                <option value="">Gương</option>
+                                <option value="">Bàn</option>
+                                <option value="">Ghế</option>
+                            </select>
+                        </td>
+                        <td>
+                            <input type="text" placeholder="Nhập tên sản phẩm" value="Giường-1">
+                        </td>
+                        <td>
+                            <input type="text" placeholder="Nhập số lượng sản phẩm" value="10">
+                        </td>
+                    </tr>
+
+                    <tr>
+                      <th>Giá</th>
+                      <th>Mô tả sản phẩm</th>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <input type="text" placeholder="Nhập giá sản phẩm" value="20.000.000">
+                        </td>
+                        <td>
+                            <input type="text" placeholder="Nhập mô tả" value="Dành cho nhà giàu">
+                        </td>
+                    </tr>
+                </table>
+                <div class="reset-form">
+                    <input type="button" id="buttonreset" style="display: none;">
+                    <label class="buttonReset" for="buttonreset">Reset</label>
+                </div>
+                <div class="submit-form" method="post">
+                    <input type="submit" id="buttonsubmit" style="display: none;" name="submit">
+                    <label class="buttonsubmit" for="buttonsubmit">Cập nhật</label>
+                </div>
             </form>
-          </div>
-          <table>
-            <tr>
-              <th>Phân loại</th>
-              <th>Tên sản phẩm</th>
-              <th>Số lượng</th>
-            </tr>
-            <tr>
-              <td>
-                <form action="">
-                  <select name="" id="" class='form-control'>
-                    <option value="">Giường</option>
-                    <option value="">Gương</option>
-                    <option value="">Bàn</option>
-                    <option value="">Ghế</option>
-                  </select>
-                </form>
-              </td>
-              <td>
-                <form action="">
-                  <input type="text" placeholder="Nhập tên sản phẩm" value="Giường-1">
-                </form>
-              </td>
-              <td>
-                <form action="">
-                  <input type="text" placeholder="Nhập số lượng sản phẩm" value="10">
-                </form>
-              </td>
 
-            </tr>
-            <tr>
-              <th>Giá</th>
-              <th>Mô tả sản phẩm</th>
-            </tr>
-            <tr>
-
-              <td>
-                <form action="">
-                  <input type="text" placeholder="Nhập giá sản phẩm" value="20.000.000">
-                </form>
-              </td>
-              <td>
-                <form action="">
-                  <input type="text" placeholder="Nhập mô tả" value="Dành cho nhà giàu">
-                </form>
-              </td>
-            </tr>
-          </table>
-          <form class="reset-form">
-            <input type="button" id="buttonreset" style="display: none;">
-            <label class="buttonReset" for="buttonreset">Reset</label>
-          </form>
-          <form class="submit-form" method="post">
-            <input type="submit" id="buttonsubmit" style="display: none;" name="submit">
-            <label class="buttonsubmit" for="buttonsubmit">Cập nhật</label>
-          </form>
-          <div class="button-back" title="Quay về trang trước">
-            <a href="products-admin.php?admin=<?php echo $username_admin?>">
-              <i class="fa-solid fa-backward-step fa-xl" style="color: black;"></i>
-            </a>
-          </div>
+            <div class="button-back" title="Quay về trang trước">
+                <a href="products-admin.php?admin=<?php echo $username_admin?>">
+                    <i class="fa-solid fa-backward-step fa-xl" style="color: black;"></i>
+                </a>
+            </div>
         </div>
       </section>
     </div>
+</body>
 
-  </body>
-
-  </html>
+</html>
