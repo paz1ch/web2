351ef8ce1c0cd18c112835d59d79351de74acdac
Dang Hung Phuc <danghungphucc@gmail.com>
2024-05-15 23:30:39 +0700

    sua

--- web/timkiem_trangchu.php
+++ web/timkiem_trangchu.php
@@ -83,7 +83,7 @@ include_once("header_user.php");
                             <div class="filter-content">
                                 <div class="card-body">
                                     <select name="type_products">
-                                        <option value="0">--chon--</option>
+                                        <option value="0">--Chọn--</option>
                                         <option value="1">Giường</option>
                                         <option value="2">Ghế</option>
                                         <option value="3">Bàn</option>
